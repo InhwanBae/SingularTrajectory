5edcd0139e842debedb6c85b929b9df6d9cd7467
Inhwan Bae <InhwanBae@users.noreply.github.com>
2024-06-10 20:31:43 +0900

    Update download_datasets.sh

--- script/download_datasets.sh
+++ script/download_datasets.sh
@@ -6,11 +6,11 @@ wget -O datasets.zip 'https://www.dropbox.com/s/8n02xqv3l9q18r1/datasets.zip?dl=
 unzip -q datasets.zip
 rm -rf datasets.zip
 
-wget -O datasets-domainextension.zip https://github.com/InhwanBae/SingularTrajectory/releases/download/v1.0/datasets-domainextension.zip
+wget -O datasets-domainextension.zip https://github.com/InhwanBae/SingularTrajectory/releases/download/v0.1/datasets-domainextension.zip
 unzip -q datasets-domainextension.zip
 rm -rf datasets-domainextension.zip
 
-wget -O datasets-imageextension.zip https://github.com/InhwanBae/SingularTrajectory/releases/download/v1.0/datasets-imageextension.zip
+wget -O datasets-imageextension.zip https://github.com/InhwanBae/SingularTrajectory/releases/download/v0.1/datasets-imageextension.zip
 unzip -q datasets-imageextension.zip
 rm -rf datasets-imageextension.zip
 
